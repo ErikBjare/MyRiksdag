0b306e60ae5aa6979dbedb9cdade0192a1bc9cca
Erik Bjäreholt <erik.bjareholt@gmail.com>
2015-05-23 12:31:52 +0200

    Added get_data.sh script

---
+++ get_data.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+mkdir -p $DIR/data/votering
+cd $DIR/data/votering
+
+download_data() {
+    declare -a years=("200203" "200304" "200405" "200506" 
+                      "200607" "200708" "200809" "200910" 
+                      "201011" "201112" "201213" "201314")
+
+    for i in "${years[@]}"
+    do
+        wget http://data.riksdagen.se/dataset/votering/votering-${i}.json.zip
+    done
+}
+
+unpack_data() {
+    for i in *.json.zip
+    do
+        SRCDIR=$(echo ${i} | grep -o "20[0-9]*")
+        mkdir $SRCDIR
+        unzip $i -d $SRCDIR
+    done
+}
+
+
+while true; do
+    read -p "Do you want to download the files? (yes/no): " yn
+    case $yn in
+        [Yy]* ) download_data; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+while true; do
+    read -p "Do you want to unpack the files? (yes/no): " yn
+    case $yn in
+        [Yy]* ) unpack_data; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
